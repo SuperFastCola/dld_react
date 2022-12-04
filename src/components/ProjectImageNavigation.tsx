3377d8f10c3eb4a437f9059267426fc7b9b67bec
Anthony Baker <anthony.baker@nexio.com>
2022-12-03 21:52:21 -0500

    Adding Image Navigation

--- src/components/ProjectImageNavigation.tsx
+++ src/components/ProjectImageNavigation.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import { KeyGenerator } from '../modules/KeyGenerator';
 
-interface Properties {
+interface props {
     sources: any;
+    currentImageIndex:number;
+    selectFunction?:(any)=>any;
 };
 
-class ProjectImageNavigation extends React.Component<Properties> {
+class ProjectImageNavigation extends React.Component<props> {
+    keyGen:KeyGenerator = new KeyGenerator();
+
+    displayNavItems(){
+       return this.props.sources.map( (item:any, index:number)=>{
+        console.log(this.props.currentImageIndex===index,this.props.currentImageIndex,index);
+            return <li className={`page-item ${(this.props.currentImageIndex===index)?"active":""}`} key={this.keyGen.createItemKey()}><a className="page-link" href="/" data-index={index} onClick={this.props.selectFunction}>{(index+1)}</a></li>
+        });
+    }
 
     render() {
-            return <div>Project Nav</div>
+            return (
+                <nav aria-label="Picture Navigation">
+                    <ul className="pagination">
+                        {this.displayNavItems()}
+                    </ul>
+                </nav>
+            )
     }
 }
 
--- src/components/ProjectsImageArea.tsx
+++ src/components/ProjectsImageArea.tsx
@@ -10,48 +10,51 @@ interface Props {
 };
 
 interface State{
-    currentImageIndex:Number;
+    currentImageIndex:number;
 }
 
 class ProjectImageArea extends React.Component<Props,State> {
     keyGen:KeyGenerator = new KeyGenerator();
-    imageAvailable:boolean = false;
 
     constructor(Props){
         super(Props);
         this.state = {currentImageIndex:0};
         this.displayImages = this.displayImages.bind(this);
         this.displayImageNav = this.displayImageNav.bind(this);
+        this.changeImage = this.changeImage.bind(this);
+    }
+
+    changeImage(e:any){
+        e.preventDefault();
+        this.setState({currentImageIndex:Number(e.target.dataset["index"])})
     }
 
     displayImages(project:any){
         //return a project image is not null
-        var img = project.image[this.props.language][0]??undefined;
-        var imageDivHolder:any = [];
+        var img = project.image[this.props.language][this.state.currentImageIndex]??undefined;
+        var imageAvailable = false;
         
         if(img!==undefined){
             
             //cycle through image object properties and determine if one of the sources is NOT null
             for(const [key,value] of Object.entries(img)){
                 if(key!=="order" && value!==null){
-                    this.imageAvailable = true;
+                    imageAvailable = true;
                 }
             }
 
             //if has images return project image object
-            if(this.imageAvailable){
-                imageDivHolder.push(<ProjectImage key={this.keyGen.createItemKey()} source={img} text={project.name[this.props.language]} path={this.props.assetPath} />);
+            if(imageAvailable){
+                return <ProjectImage key={this.keyGen.createItemKey()} source={img} text={project.name[this.props.language]} path={this.props.assetPath} />;
             }
 
-            return (imageDivHolder.length>0)?imageDivHolder:null;
+            return null;
         }
     }
 
     displayImageNav(project:any){
-        if(this.imageAvailable && project[this.props.language]!==undefined){
-            if(project[this.props.language].length>1){
-                return <div>Project Nav</div>;
-            }
+        if(project.image[this.props.language]!==undefined && project.image[this.props.language].length>1){
+                return <ProjectImageNavigation sources={project.image[this.props.language]} currentImageIndex={this.state.currentImageIndex} selectFunction={this.changeImage} />
         }
     }
 
